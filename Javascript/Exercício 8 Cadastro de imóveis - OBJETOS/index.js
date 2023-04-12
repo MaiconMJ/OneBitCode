89e6f552eee10069d1adb646fa88e27ff3571b20
Maicon Miranda de Jesus <maicon.miranda77@hotmail.com>
2023-04-11 21:11:26 -0300

    Iniciado o exercício 8

--- Javascript/Exercício 8 Cadastro de imóveis - OBJETOS/index.js
+++ Javascript/Exercício 8 Cadastro de imóveis - OBJETOS/index.js
@@ -1,6 +1,27 @@
-const menu = ''
+let opcao = ''
+let imoveisCadastrados = []
 
 do {
-  alert("")
 
-} while ()
+  let imovel = {}
+  for (let i = 0; i < imoveisCadastrados.length; i++) {
+    imovel += (i + 1) + 'imóveis cadastrados'
+  }
+
+  opcao = prompt("Selecione uma opção:\n\n" +
+  "1. Salvar imóvel\n" + "2. Consultar imóveis cadastrados\n" + 
+  "3. Sair")
+
+  switch (opcao) {
+    case '1':
+      let novoImovel = {}
+      novoImovel.nome = prompt('Informe o nome do proprietário:')
+      novoImovel.quartos = prompt('Infome a quantidade de quartos:')
+      novoImovel.banheiros = prompt('Informe a quantidade de banheiros:')
+      novoImovel.garagem = prompt('Possui garagem? Sim/Não')
+      break
+    case '2':
+  }
+
+
+} while (opcao !== "3")
