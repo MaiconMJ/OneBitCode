bf26ad0623510125944f72073be35596c3e37acb
Maicon Miranda de Jesus <maicon.miranda77@hotmail.com>
2023-05-15 21:34:04 -0300

    adicionada a funcao exibirMenu

---
+++ Javascript/Exercício 10 Desafio Final/index.js
@@ -0,0 +1,12 @@
+function exibirMenu() {
+  return prompt(
+    'Selecione uma opção: \n' +
+    '1. Listar vagas disponíveis\n' +
+    '2. Criar um nova vaga\n' +
+    '3. Visualizar uma vaga\n' +
+    '4. Inscrever um candidato em uma vaga\n' +
+    '5. Excluir uma vaga\n' +
+    '6. Sair\n'
+  )
+}
+
