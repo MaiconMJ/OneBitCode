1237f24f6caa83577db0b642e3f50ca28d1521be
Maicon Miranda de Jesus <maicon.miranda77@hotmail.com>
2023-05-16 20:52:33 -0300

    Iniciando a função visualizarVaga

--- Javascript/Exercício 10 Desafio Final/index.js
+++ Javascript/Exercício 10 Desafio Final/index.js
@@ -19,9 +19,21 @@ function criarVaga() {
   }
 }
 
+function VisualizarVaga() {
+  for (let i = 0; i < vagas.length; i++) {
+    prompt('Informe o índice da vaga: ')
+    alert(
+      'Vaga ' + (i + 1) +
+      '\n' + vagas[i].nome +
+      '\n' + vagas[i].descricao +
+      '\n' + vagas[i].dataLimite
+    )
+  }
+}
+
 function exibirMenu() {
   return prompt(
-    "Bem-vindo ao Cadastro de Vagas.\nTotal de vagas: " + vagas.lenght +
+    'Bem-vindo ao Cadastro de Vagas.\nTotal de vagas: ' + vagas.lenght +
     'Selecione uma opção: \n' +
     '1. Listar vagas disponíveis\n' +
     '2. Criar um nova vaga\n' +
