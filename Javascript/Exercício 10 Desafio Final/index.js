9090f829f19632cec3ce6e57edde14456e21dca1
Maicon Miranda de Jesus <maicon.miranda77@hotmail.com>
2023-05-25 20:13:19 -0300

    refazendo o exercício

--- Javascript/Exercício 10 Desafio Final/index.js
+++ Javascript/Exercício 10 Desafio Final/index.js
@@ -1,79 +1,19 @@
-const vagas = []
+let opcao = ""
 
-function criarVaga() {
+do {
+  opcao = prompt(
+    'Bem-vindo ao Cadastro de Vagas.\n\n' + 
+    
+    'Total de vagas: \n' +
 
-  const vaga = {}
-
-  vaga.nome = prompt("Informe o nome da vaga: ")
-  vaga.descricao = prompt("Descreva a vaga: ")
-  vaga.dataLimite = prompt("Informe a data limite para se candidatar: ")
-
-  const confirma = confirm(
-    "Por favor, confirme se os dados estão corretos:\n" + 
-    "\nNome da vaga: " + vaga.nome + 
-    "\nDescrição: " + vaga.descricao + 
-    "\nData limite: " + vaga.dataLimite)
-
-  if (confirma) {
-    vagas.push(vaga)
-  }
-}
-
-// function visualizarVaga() {
-//   for (let i = 0; i < vagas.length; i++) {
-//     prompt('Informe o índice da vaga: ')
-//     alert(
-//       'Vaga ' + (i + 1) +
-//       '\n' + vagas[i].nome +
-//       '\n' + vagas[i].descricao +
-//       '\n' + vagas[i].dataLimite
-//     )
-//   }
-// }
-
-function exibirMenu() {
-  return prompt(
-    'Bem-vindo ao Cadastro de Vagas.\nTotal de vagas: ' + vagas.lenght +
-    'Selecione uma opção: \n' +
-    '1. Listar vagas disponíveis\n' +
-    '2. Criar uma nova vaga\n' +
-    '3. Visualizar uma vaga\n' +
-    '4. Inscrever um candidato em uma vaga\n' +
-    '5. Excluir uma vaga\n' +
-    '6. Sair\n'
+    '\nSelecione uma opção: ' +
+    '\n1. Listar vagas disponíveis' +
+    '\n2. Criar uma nova vaga' +
+    '\n3. Visualizar uma vaga' +
+    '\n4. Inscrever um candidato em uma vaga' +
+    '\n5. Excluir uma vaga' +
+    '\n6. Sair'
   )
-}
 
-function executar() {
-  let opcao = ''
-
-  do {
-    opcao = exibirMenu()
-    let resultado
-
-    switch (opcao) {
-      case '':
-        
-    }
-    switch (opcao) {
-      case '':
-
-    }
-    switch (opcao) {
-      case '':
-
-    }
-    switch (opcao) {
-      case '':
-
-    }
-    switch (opcao) {
-      case '':
-
-    }
-    switch (opcao) {
-      case '':
-
-    }
-  } while (opcao !== '6')
-} 
+  
+} while (opcao !== "6")
