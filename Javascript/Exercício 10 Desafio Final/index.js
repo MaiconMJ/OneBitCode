5ee2ad4cf6191ba466bf4ce07e9f555d0fc86c14
Maicon Miranda de Jesus <maicon.miranda77@hotmail.com>
2023-05-25 20:44:12 -0300

    Listar vagas iniciado

--- Javascript/Exercício 10 Desafio Final/index.js
+++ Javascript/Exercício 10 Desafio Final/index.js
@@ -1,10 +1,11 @@
 let opcao = ""
+const vagas = []
 
 do {
   opcao = prompt(
     'Bem-vindo ao Cadastro de Vagas.\n\n' + 
     
-    'Total de vagas: \n' +
+    'Total de vagas: \n' + vagas.length +
 
     '\nSelecione uma opção: ' +
     '\n1. Listar vagas disponíveis' +
@@ -15,5 +16,39 @@ do {
     '\n6. Sair'
   )
 
-  
+  switch(opcao) {
+    case "1":
+      for (let i = 0; i < vagas.length; i++){
+        alert(
+          'Vaga ' + (i + 1) +
+          '\nNome: ' + vagas[i].nome +
+          '\nNúmero de candidatos: ' 
+
+          )
+      }
+    break
+
+    case "2":
+      
+
+    break
+
+    case "3":
+
+    break
+
+    case "4":
+
+    break
+
+    case "5":
+
+    break
+
+    case "6":
+      alert("saindo...")
+    break
+  }
+
+
 } while (opcao !== "6")
