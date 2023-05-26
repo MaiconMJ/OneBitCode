934b8df8e304bbd9f2619d6e28e33ee034f19559
Maicon Miranda de Jesus <maicon.miranda77@hotmail.com>
2023-05-26 13:29:35 -0300

    Voltando com o código original

---
+++ Javascript/Exercício 10 Desafio Final/index.js
@@ -0,0 +1,85 @@
+let opcao = ""
+const vagas = []
+
+do {
+  opcao = prompt(
+    'Bem-vindo ao Cadastro de Vagas.\n\n' + 
+    
+    'Total de vagas: ' + vagas.length +
+
+    '\nSelecione uma opção: ' +
+    '\n1. Listar vagas disponíveis' +
+    '\n2. Criar uma nova vaga' +
+    '\n3. Visualizar uma vaga' +
+    '\n4. Inscrever um candidato em uma vaga' +
+    '\n5. Excluir uma vaga' +
+    '\n6. Sair'
+  )
+
+  switch(opcao) {
+    case "1":
+      for (let i = 0; i < vagas.length; i++){
+        alert(
+          'Vaga ' + (i + 1) +
+          '\nNome: ' + vagas[i].nome //+
+          //'\nNúmero de candidatos: ' + vagas.candidato.length
+          )
+      }
+      if(vagas.length === 0) {
+        alert('Não há vagas cadastradas.')
+      }
+
+      // const vagasDisponiveis = vagas.map(function (vaga) {
+      //   return (
+      //     vaga.nome,
+      //     vaga.candidatos
+      //   )
+      // })
+      // alert(vagasDisponiveis)
+      
+    break
+
+    case "2":
+      const novaVaga = {}
+      
+      novaVaga.nome = prompt('Digite o nome da vaga: ')
+      novaVaga.descricao = prompt('Descreva a vaga: ')
+      novaVaga.dataLimite = prompt('Digite a data limite para se candidatar: ')
+
+      const confirmacao = confirm(
+        'Os dados estão corretos?\n' +
+        '\nNome da vaga: ' + novaVaga.nome +
+        '\nDescrição da vaga: ' + novaVaga.descricao +
+        '\nData limite: ' + novaVaga.dataLimite 
+      )
+
+      if (confirmacao) {
+        vagas.push(novaVaga)
+        alert('Vaga adicionada com sucesso!')
+      } else {
+        alert('cadastro cancelado')
+      }
+    break
+
+    case "3":
+      
+    break
+
+    case "4":
+      
+
+      
+
+    break
+
+    case "5":
+
+    break
+
+    case "6":
+      alert("saindo...")
+    break
+  }
+
+
+} while (opcao !== "6")
