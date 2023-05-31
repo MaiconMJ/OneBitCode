c5646e1c357987ab62a569dee4452a8cf1136022
Maicon Miranda de Jesus <maicon.miranda77@hotmail.com>
2023-05-30 21:59:41 -0300

    Ainda não está funcionando

--- Javascript II - DOM/Exercício 11 Escalação de Times/index.js
+++ Javascript II - DOM/Exercício 11 Escalação de Times/index.js
@@ -1,9 +1,23 @@
 function escalarJogador () {
 
-  const convocados = document.getElementById('area-dos-convocados')
+  const position = document.getElementById("position").value
+  const name = document.getElementById("name").value
+  const number = document.getElementById('number').value
 
-  const ul = document.createElement('ul')
+  const confirmation = cfirm('Escalar ' + name + ' como ' + position + "?")
 
-  
+  if (confirmation) {
+    
+    const jogadoresConvocados = document.getElementById("convocados")
+    const jogador = document.createElement('li')
+    jogador.id = "player-" + number
+    jogador.innerText = position + ": " + name + " (" + number + ")"
+    jogadoresConvocados.appendChild(jogador)
+
+    document.getElementById("position").value = ""
+    document.getElementById("name").value = ""
+    document.getElementById("number").value = ""
+
+  }
 
 }
