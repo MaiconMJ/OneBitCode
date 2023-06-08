444c5331386477dd642d4ca1cb13906d9fe1a298
Maicon Miranda de Jesus <maicon.miranda77@hotmail.com>
2023-06-07 21:26:10 -0300

    Adicionado o alert para exibir os dados das operações

--- Javascript/Exercício 1 Cadastro de usuário - Calculadora de 4 operações/calculadora.js
+++ Javascript/Exercício 1 Cadastro de usuário - Calculadora de 4 operações/calculadora.js
@@ -8,3 +8,11 @@ const soma = x + y
 const subtracao = x - y
 const multiplicacao = x * y
 const divisao = x / y
+
+alert(
+  "Resultados:\n" +
+  "\nSoma: " + soma +
+  "\nSubtração: " + subtracao +
+  "\nMultiplicação: " + multiplicacao +
+  "\nDivisão: " + divisao
+)
