f7ef84dd4ab8c235ff54529833f682a0a06af8f6
Maicon Miranda de Jesus <maicon.miranda77@hotmail.com>
2023-06-13 13:48:29 -0300

    Exercício finalizado

---
+++ Javascript II - DOM/aula 10 - Enviando Valores do DOM para o Javascript/index.js
@@ -0,0 +1,13 @@
+function register (element) {
+
+  const username = element.children.username.value
+  const password = element.children.password.value
+  const passwordConfirmation = element.children.passwordConfirmation.value
+
+  if (password === passwordConfirmation) {
+    alert("Usuário(a) " + username + " registrado!")
+  } else {
+    alert("As senhas não conferem")
+  }
+
+}
