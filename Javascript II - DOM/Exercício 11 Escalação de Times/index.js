513419a676ca2946b0d16517adf89557dd9b62ad
Maicon Miranda de Jesus <maicon.miranda77@hotmail.com>
2023-06-13 12:33:44 -0300

    Exercício finalizado

--- Javascript II - DOM/Exercício 11 Escalação de Times/index.js
+++ Javascript II - DOM/Exercício 11 Escalação de Times/index.js
@@ -1,35 +1,35 @@
 function escalarJogador () {
 
-  const position = document.getElementById("position").value
-  const name = document.getElementById("name").value
-  const number = document.getElementById('number').value
+  const position = document.getElementById("position").value  //Pegando o valor do input e guaradando na variável
+  const name = document.getElementById("name").value          //Pegando o valor do input e guaradando na variável
+  const number = document.getElementById('number').value      //Pegando o valor do input e guaradando na variável
 
-  const confirmation = confirm('Escalar ' + name + ' como ' + position + "?")
+  const confirmation = confirm('Escalar ' + name + ' como ' + position + "?") //Confirmação
 
-  if (confirmation) {
+  if (confirmation) {   //Se confirmado...
     
-    const jogadoresConvocados = document.getElementById("convocados")
-    const jogador = document.createElement('li')
+    const jogadoresConvocados = document.getElementById("convocados")  //Ambiente onde serão exibidos os jogadores - ul.
+    const jogador = document.createElement('li')  //Criando a li jogador
     jogador.id = "player-" + number
-    jogador.innerText = position + ": " + name + " (" + number + ")"
-    jogadoresConvocados.appendChild(jogador)
+    jogador.innerText = position + ": " + name + " (" + number + ")" //O que será exibido
+    jogadoresConvocados.appendChild(jogador)      //jogador(li) é filho de joogadoredConvocados(ul)
 
-    document.getElementById("position").value = ""
-    document.getElementById("name").value = ""
-    document.getElementById("number").value = ""
+    document.getElementById("position").value = ""  //limpando os inputs
+    document.getElementById("name").value = ""      //limpando os inputs
+    document.getElementById("number").value = ""    //limpando os inputs
 
   }
 
 }
 
-function removePlayer() {
-  const number = document.getElementById("numberToRemove").value
-  const playerToRemove = document.getElementById("player-" + number)
+function removerJogador() {
+  const number = document.getElementById("numberToRemove").value  //Pegando o valor do input e guaradando na variável
+  const playerToRemove = document.getElementById("player-" + number)  //id informado em jogador.id - linha 13
 
-  const confirmation = confirm("Remover o jogador " + playerToRemove.innerText + "?")
+  const confirmation = confirm("Remover o jogador " + playerToRemove.innerText + "?") //Confirmação. // playerToRemove.innerText : retorna o valor passado pelo usuário
 
-  if (confirmation) {
-    document.getElementById("team-list").removeChild(playerToRemove)
-    document.getElementById("numberToRemove").value = ""
+  if (confirmation) {     //Se confirmado
+    document.getElementById("convocados").removeChild(playerToRemove) //remove o jogador
+    document.getElementById("numberToRemove").value = ""  //limpando o input
   }
 }
