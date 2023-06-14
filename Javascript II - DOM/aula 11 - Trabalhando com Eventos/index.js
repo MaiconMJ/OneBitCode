4ee3f8322fda745a056e5d0a008ab92f5ad8f606
Maicon Miranda de Jesus <maicon.miranda77@hotmail.com>
2023-06-14 20:21:35 -0300

    Adicionados comentários

--- Javascript II - DOM/aula 11 - Trabalhando com Eventos/index.js
+++ Javascript II - DOM/aula 11 - Trabalhando com Eventos/index.js
@@ -13,15 +13,15 @@ function register(ev) {
 
 }
 
-const button = document.getElementById('register-button')
+const button = document.getElementById('register-button') 
 
-button.addEventListener('click', register) 
+button.addEventListener('click', register) //Adicionando um evento
 
 button.addEventListener('mouseover', function (ev) {  //Exemplo de outro tipo de evento: mouseover. \\ Dessa vez a função foi passada diretamente
   console.log(ev)
 })
 
-function removeEvent () {
+function removeEvent () {                //Removendo um evento. Neste caso o evento do tipo 'click' da função register
   button.removeEventListener('click', register)
   alert("Evento removido!")
 }
