24b1b412290551b9a16aa84d708a0cd55f109e7d
Maicon Miranda de Jesus <maicon.miranda77@hotmail.com>
2023-06-14 21:16:35 -0300

    Exercício da aula trabalhando com formulários ok

---
+++ Javascript II - DOM/aula 12 - Trabalhando com Formulários no Javascript/index.js
@@ -0,0 +1,35 @@
+const form = document.getElementById('orderForm')
+
+form.addEventListener('submit', function (ev) {
+  ev.preventDefault()
+
+  const name = document.querySelector("input[name='name']").value
+  const address = document.querySelector("input[name='address']").value
+  const breadType = document.querySelector("select[name='breadType']").value
+  const main = document.querySelector("input[name='main']").value
+
+  let salad = ''
+  document.querySelectorAll("input[name='salad']:checked").forEach(function (item) {
+    salad += " - " + item.value + "\n"
+  })
+
+  const observations = document.querySelector("textarea[name='observations']").value
+
+  console.log({
+    name,
+    address,
+    breadType,
+    main,
+    salad,
+    observations
+  })
+
+  alert(
+    "Pedido Realizado!" +
+    "\nNome do(a) cliente: " + name +
+    "\nEndereço de entrega: " + address +
+    "\nTipo de pão: " + breadType +
+    "\nRecheio: \n - " + main + "\n" + salad +
+    "Observações: " + observations
+  )
+})
