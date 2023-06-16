8d9290336d9196517433e97c0c67937dd8c9651f
Maicon Miranda de Jesus <maicon.miranda77@hotmail.com>
2023-06-15 21:25:33 -0300

    adicionados os radio inputs

--- Javascript II - DOM/Exercício 12 Cadastro de devs/index.js
+++ Javascript II - DOM/Exercício 12 Cadastro de devs/index.js
@@ -31,7 +31,20 @@ addTechBtn.addEventListener('click', function (ev) {
 
   const techNameLabel = createLabel('Nome: ', 'techName-' + rowIndex)
   const techNameInput = createInput('techName-' + rowIndex, null, 'techName')
+  
 
+  const expLabel1 = createLabel('Experiência: ')
+  const id1 = 'expRadio-' + rowIndex + '.1'
+  const expRadioInput1= createInput(id1, '0-2 anos', 'techExp-' + rowIndex, 'radio')
+
+  const expLabel2 = createLabel('Experiência: ')
+  const id2 = 'expRadio-' + rowIndex + '.2'
+  const expRadioInput2 = createInput(id2, '3-4 anos', 'techExp-' + rowIndex, 'radio')
+
+  const expLabel3 = createLabel('Experiência: ')
+  const id3 = 'expRadio-' + rowIndex + '.3'
+  const expRadioInput3 = createInput(id3, '5+ anos', 'techExp-' + rowIndex, 'radio')
+ 
   newRow.append(techNameLabel, techNameInput)
 
   stackIputs.appendChild(newRow)
