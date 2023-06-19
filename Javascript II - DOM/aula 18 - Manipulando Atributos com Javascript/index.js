b1b2e58c0d2d89c3e7115ceeb82069b32c29cbc5
Maicon Miranda de Jesus <maicon.miranda77@hotmail.com>
2023-06-19 10:37:19 -0300

    Funcão de teste alterando o value ok

--- Javascript II - DOM/aula 18 - Manipulando Atributos com Javascript/index.js
+++ Javascript II - DOM/aula 18 - Manipulando Atributos com Javascript/index.js
@@ -2,4 +2,7 @@ const input = document.getElementById('input')
 
 document.getElementById('value').addEventListener('click', function () {
   input.value = 'Olá, mundo'
+
+  console.log(input.value)
+  console.log(input.getAttribute('value'))
 })
