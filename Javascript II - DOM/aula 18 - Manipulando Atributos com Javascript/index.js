6c8a7dcb8ecb287db24455d477b2427b34f213ec
Maicon Miranda de Jesus <maicon.miranda77@hotmail.com>
2023-06-19 10:49:22 -0300

    Funcão de teste alterando o type ok

--- Javascript II - DOM/aula 18 - Manipulando Atributos com Javascript/index.js
+++ Javascript II - DOM/aula 18 - Manipulando Atributos com Javascript/index.js
@@ -6,3 +6,8 @@ document.getElementById('value').addEventListener('click', function () {
   console.log(input.value)
   console.log(input.getAttribute('value'))
 })
+
+document.getElementById('type').addEventListener('click', function () {
+  input.type = input.type !== 'radio' ? 'radio' : 'text'
+  //input.setAttribute('type', 'radio') // Outra opção, setando o atributo diretamente // Opção de mão única
+})
