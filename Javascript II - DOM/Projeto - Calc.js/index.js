e756a484f579abcc6e5cf55a039fe25ea53d6ea7
Maicon Miranda de Jesus <maicon.miranda77@hotmail.com>
2023-06-22 21:27:06 -0300

    input allowed keys ok

---
+++ Javascript II - DOM/Projeto - Calc.js/index.js
@@ -0,0 +1,41 @@
+//Funcionalidade: digitar na calculadora
+
+//Selecionando alguns elementos:
+
+
+//QuerySelector:
+//Retorna o primeiro elemento dentro do documento que corresponde ao grupo especificado de seletores.
+//Seleciona o elemento como se fosse o css
+//Ex: ('myTag'), ('#my-Id'), ('.my-class')
+
+const main = document.querySelector('main') 
+const root = document.querySelector(':root')            //Da mesma forma que foi declarado no CSS
+const input = document.getElementById('input')
+const resultInput = document.getElementById('result')
+
+//Impedindo que o usuário digite outros caracteres além dos permitidos na calculadora:
+
+//Array com todos os caracteres permitidos:
+
+const allowedKeys = ["(", ")", "/", "*", "-", "+", "9", "8", "7", "6", "5", "4", "3", "2", "1", "0", ".", "%", " "]
+
+
+//Adicionando o evento de quando a tecla é pressionada:
+
+input.addEventListener("keydown", function (ev) {
+  ev.preventDefault()                                 //Impedindo o comportamento padrão do input 
+  if (allowedKeys.includes(ev.key)) {                 //ev.keys : tecla/evento que o usuário disparou
+    input.value += ev.key  
+    return                                            //Se dentro dos válidos, acrescenta o caracter ao value do input
+  }
+  if (ev.key === 'Backspace') {                       //verifica se a key pressionada é o Backspace, se sim atribui a ela o método de 
+    input.value = input.value.slice(0, -1)            //remover(apagar) slice(0, -1) : a partir do caracter 0, remove o ultimo elemento
+  }
+  if (ev.key === 'Enter') {
+    calculate()
+  }
+})
+
+function calculate () {
+  console.log('calculado!')
+}
