85c1951f02a72ba0ed889c2f0912e33b96325f3b
Maicon Miranda de Jesus <maicon.miranda77@hotmail.com>
2023-06-28 20:59:23 -0300

    Função switchPlayer ok, botões x e o ok

--- Javascript-II-DOM/Projeto-Tic-Tac-Toe--Jogo-da-Velha/index.js
+++ Javascript-II-DOM/Projeto-Tic-Tac-Toe--Jogo-da-Velha/index.js
@@ -1,13 +1,32 @@
-const input1 = document.getElementById('name').value 
+const main = document.querySelector('main') 
+const root = document.querySelector(':root')     
+const input1 = document.getElementById('name').value
 const input2 = document.getElementById('name2').value
 
 
-document.querySelectorAll('.gameBtn').forEach(function (gameButton) {
-  gameButton.addEventListener('click', function() {
-    if (gameButton.innerText === '') {
-      gameButton.innerText = 'x'
-    }
-  })
+document.getElementById('switchPlayer').addEventListener('click', function () {
+  if (main.dataset.player === 'player1') {                          
+    // root.style.setProperty('--bg-color', '#f1f5f9')              
+    // root.style.setProperty('--border-color', '#aaa')
+    // root.style.setProperty('--font-color', '#212529')
+    root.style.setProperty('--primary-color', '#26834a')
+    main.dataset.player = 'player2'
+  } else {
+    // root.style.setProperty('--bg-color', '#212529')             
+    // root.style.setProperty('--border-color', '#666')
+    // root.style.setProperty('--font-color', '#f1f5f9')
+    root.style.setProperty('--primary-color', '#4dff91')
+    main.dataset.player = 'player1'
+  }                                                   
 })
 
 
+document.querySelectorAll('.gameBtn').forEach(function (gameButton) {
+  gameButton.addEventListener('click', function () {
+    if (main.dataset.player === 'player1') {
+      gameButton.innerText = 'X'
+    } else {
+      gameButton.innerText = 'O'
+    }
+  })
+})
