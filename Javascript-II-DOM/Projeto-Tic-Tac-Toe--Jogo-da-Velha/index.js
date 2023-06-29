f024608e46057c43d38cbf83c564fbb7d12a3a5e
Maicon Miranda de Jesus <maicon.miranda77@hotmail.com>
2023-06-28 21:18:07 -0300

    Alterada a cor primária

--- Javascript-II-DOM/Projeto-Tic-Tac-Toe--Jogo-da-Velha/index.js
+++ Javascript-II-DOM/Projeto-Tic-Tac-Toe--Jogo-da-Velha/index.js
@@ -9,13 +9,13 @@ document.getElementById('switchPlayer').addEventListener('click', function () {
     // root.style.setProperty('--bg-color', '#f1f5f9')              
     // root.style.setProperty('--border-color', '#aaa')
     // root.style.setProperty('--font-color', '#212529')
-    root.style.setProperty('--primary-color', '#26834a')
+    root.style.setProperty('--primary-color', '#914dff')
     main.dataset.player = 'player2'
   } else {
     // root.style.setProperty('--bg-color', '#212529')             
     // root.style.setProperty('--border-color', '#666')
     // root.style.setProperty('--font-color', '#f1f5f9')
-    root.style.setProperty('--primary-color', '#4dff91')
+    root.style.setProperty('--primary-color', '#4d62ff')
     main.dataset.player = 'player1'
   }                                                   
 })
@@ -23,10 +23,11 @@ document.getElementById('switchPlayer').addEventListener('click', function () {
 
 document.querySelectorAll('.gameBtn').forEach(function (gameButton) {
   gameButton.addEventListener('click', function () {
-    if (main.dataset.player === 'player1') {
+    if (main.dataset.player === 'player1' && gameButton.innerText === '') {
       gameButton.innerText = 'X'
-    } else {
+    } else if (main.dataset.player === 'player2' && gameButton.innerText === '') {
       gameButton.innerText = 'O'
     }
+
   })
 })
