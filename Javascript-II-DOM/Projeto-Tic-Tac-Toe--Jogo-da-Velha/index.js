0292bb29064f939182fe0c7775789e9fb2e83c53
Maicon Miranda de Jesus <maicon.miranda77@hotmail.com>
2023-07-13 16:18:20 -0300

    adicionada a const boardRegion

--- Javascript-II-DOM/Projeto-Tic-Tac-Toe--Jogo-da-Velha/index.js
+++ Javascript-II-DOM/Projeto-Tic-Tac-Toe--Jogo-da-Velha/index.js
@@ -1,3 +1,4 @@
+const boardRegion = document.querySelectorAll('#gameBoard span')
 const section = document.querySelector('section') 
 const root = document.querySelector(':root')     
 let turnPlayer = ''
@@ -11,36 +12,30 @@ function initializeGame () {
   turnPlayer = 'player1'
   document.querySelector('h2').innerHTML = 'Vez de: <span id="turnPlayer"></span>'
   updateTitle()
+  
 }
 
 document.getElementById('start').addEventListener('click', initializeGame)
 
 
-
-// document.getElementById('switchPlayer').addEventListener('click', function () {
-//   if (section.dataset.player === 'player1') {                          
-//     // root.style.setProperty('--bg-color', '#f1f5f9')              
-//     // root.style.setProperty('--border-color', '#aaa')
-//     // root.style.setProperty('--font-color', '#212529')
-//     root.style.setProperty('--primary-color', '#914dff')
-//     section.dataset.player = 'player2'
-//   } else {
-//     // root.style.setProperty('--bg-color', '#212529')             
-//     // root.style.setProperty('--border-color', '#666')
-//     // root.style.setProperty('--font-color', '#f1f5f9')
-//     root.style.setProperty('--primary-color', '#4d62ff')
-//     section.dataset.player = 'player1'
-//   }                                                   
-// })
+document.getElementById('switchPlayer').addEventListener('click', function () {
+  if (section.dataset.player === 'player1') {                          
+    root.style.setProperty('--primary-color', '#914dff')
+    section.dataset.player = 'player2'
+  } else {
+    root.style.setProperty('--primary-color', '#4d62ff')
+    section.dataset.player = 'player1'
+  }                                                   
+})
 
 
-// document.querySelectorAll('.cursor-pointer').forEach(function (gameButton) {
-//   gameButton.addEventListener('click', function () {
-//     if (section.dataset.player === 'player1' && gameButton.innerText === '') {
-//       gameButton.innerText = 'X'
-//     } else if (section.dataset.player === 'player2' && gameButton.innerText === '') {
-//       gameButton.innerText = 'O'
-//     }
-//   })
-// })
+document.querySelectorAll('.cursor-pointer').forEach(function (element) {
+  element.addEventListener('click', function () {
+    if (section.dataset.player === 'player1' && element.innerText === '') {
+      element.innerText = 'X'
+    } else if (section.dataset.player === 'player2' && element.innerText === '') {
+      element.innerText = 'O'
+    }element
+  })
+})
 
