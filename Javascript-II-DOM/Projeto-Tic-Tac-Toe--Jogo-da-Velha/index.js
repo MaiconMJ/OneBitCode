3a80727d58cb6a3dbdf683f443ecd179acc0439a
Maicon Miranda de Jesus <maicon.miranda77@hotmail.com>
2023-07-17 19:17:19 -0300

    Adicionada a função getWinRegions()

--- Javascript-II-DOM/Projeto-Tic-Tac-Toe--Jogo-da-Velha/index.js
+++ Javascript-II-DOM/Projeto-Tic-Tac-Toe--Jogo-da-Velha/index.js
@@ -24,3 +24,24 @@ function initializeGame() {
   })
 }
 
+//Verifica se existem três regiões iguais em sequência e devolve as regiões
+function getWinRegions() {
+  const winRegions = []
+  if (vBoard[0][0] && vBoard[0][0] === vBoard[0][1] && vBoard[0][0] === vBoard[0][2])
+    winRegions.push("0.0", "0.1", "0.2")
+  if (vBoard[1][0] && vBoard[1][0] === vBoard[1][1] && vBoard[1][0] === vBoard[1][2])
+    winRegions.push("1.0", "1.1", "1.2")
+  if (vBoard[2][0] && vBoard[2][0] === vBoard[2][1] && vBoard[2][0] === vBoard[2][2])
+    winRegions.push("2.0", "2.1", "2.2")
+  if (vBoard[0][0] && vBoard[0][0] === vBoard[1][0] && vBoard[0][0] === vBoard[2][0])
+    winRegions.push("0.0", "1.0", "2.0")
+  if (vBoard[0][1] && vBoard[0][1] === vBoard[1][1] && vBoard[0][1] === vBoard[2][1])
+    winRegions.push("0.1", "1.1", "2.1")
+  if (vBoard[0][2] && vBoard[0][2] === vBoard[1][2] && vBoard[0][2] === vBoard[2][2])
+    winRegions.push("0.2", "1.2", "2.2")
+  if (vBoard[0][0] && vBoard[0][0] === vBoard[1][1] && vBoard[0][0] === vBoard[2][2])
+    winRegions.push("0.0", "1.1", "2.2")
+  if (vBoard[0][2] && vBoard[0][2] === vBoard[1][1] && vBoard[0][2] === vBoard[2][0])
+    winRegions.push("0.2", "1.1", "2.0")
+  return winRegions
+}
