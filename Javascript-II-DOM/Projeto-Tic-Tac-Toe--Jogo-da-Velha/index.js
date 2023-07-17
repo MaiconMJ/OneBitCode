050f1dc088b181572d5a7293afcaab821b3cf856
Maicon Miranda de Jesus <maicon.miranda77@hotmail.com>
2023-07-17 19:08:51 -0300

    Adicionada a função updateTitle()

--- Javascript-II-DOM/Projeto-Tic-Tac-Toe--Jogo-da-Velha/index.js
+++ Javascript-II-DOM/Projeto-Tic-Tac-Toe--Jogo-da-Velha/index.js
@@ -1,4 +1,9 @@
-//Variáveis globais úteis
+// Variáveis globais úteis
 const boardRegions = document.querySelectorAll('#gameBoard span')
 let vBoard = []
 let turnPlayer = ''
+
+function updateTitle () {
+  const playerInput = document.getElementById(turnPlayer)
+  document.getElementById('turnPlayer').innerText = playerInput.value
+}
