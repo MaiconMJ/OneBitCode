63b70fc54bc30493c73120d5a6f7f9a8a697d34e
Maicon Miranda de Jesus <maicon.miranda77@hotmail.com>
2023-07-17 19:14:52 -0300

    Adicionada a função initializeGame()

--- Javascript-II-DOM/Projeto-Tic-Tac-Toe--Jogo-da-Velha/index.js
+++ Javascript-II-DOM/Projeto-Tic-Tac-Toe--Jogo-da-Velha/index.js
@@ -3,7 +3,24 @@ const boardRegions = document.querySelectorAll('#gameBoard span')
 let vBoard = []
 let turnPlayer = ''
 
-function updateTitle () {
+function updateTitle() {
   const playerInput = document.getElementById(turnPlayer)
   document.getElementById('turnPlayer').innerText = playerInput.value
 }
+
+function initializeGame() {
+  // Inicializa as variáveis globais
+  vBoard = [['', '', ''], ['', '', ''], ['', '', '']]
+  turnPlayer = 'player1'
+  // Ajusta o título da página (caso seja necessário)
+  document.querySelector('h2').innerHTML = 'Vez de: <span id="turnPlayer"></span>'
+  updateTitle()
+  // Limpa o tabuleiro (caso seja necessário) e adiciona os eventos clique
+  boardRegions.forEach(function (element) {
+    element.classList.remove('win')
+    element.innerText = ''
+    element.classList.add('cursor-pointer')
+    element.addEventListener('click', handleBoardClick)
+  })
+}
+
