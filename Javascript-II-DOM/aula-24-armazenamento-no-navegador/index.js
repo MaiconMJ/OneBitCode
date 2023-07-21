32d066271b3075f7802da3d4e7eacce115b6c0a8
Maicon Miranda de Jesus <maicon.miranda77@hotmail.com>
2023-07-20 23:13:20 -0300

    função localStorage.setItem ok

--- Javascript-II-DOM/aula-24-armazenamento-no-navegador/index.js
+++ Javascript-II-DOM/aula-24-armazenamento-no-navegador/index.js
@@ -10,3 +10,11 @@ document.getElementById('readSession').addEventListener('click', function () {
   const info = sessionStorage.getItem('nomeDaChave')
   alert('A informação salva é: ' + info)
 })
+
+// Usando o Local Storage
+
+document.getElementById('local').addEventListener('click', function () {
+  const input = document.getElementById('local')
+  localStorage.setItem('nomeDaChave', input.value)
+  input.value = ''
+})
