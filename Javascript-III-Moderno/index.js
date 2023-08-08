735072066d8fca96406819d23b9718f5256dc0fb
Maicon Miranda de Jesus <maicon.miranda77@hotmail.com>
2023-08-07 21:39:27 -0300

    Função da média ponderada ok

--- Javascript-III-Moderno/index.js
+++ Javascript-III-Moderno/index.js
@@ -4,3 +4,16 @@ const average = (...numbers) => {
 }
 
 console.log(`Média Aritmética Simples: ${average(3, 6, 10, 9)}`)
+
+
+const weightedAverage = (...entries) => {
+  const sum = entries.reduce((accum, { number, weight }) => accum + (number * (weight ?? 1)), 0)
+  const weightSum = entries.reduce((accum, entry) => accum + (entry.weight ?? 1), 0)
+  return sum / weightSum
+}
+
+console.log(`Média Ponderada: ${weightedAverage(
+  { number: 9, weight: 3 },
+  { number: 7 },
+  { number: 10, weight: 1 },
+)}`)
